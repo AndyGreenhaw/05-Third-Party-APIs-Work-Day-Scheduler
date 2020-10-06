1499f4a6da1cc7599e9b93debaafe0ad54bc1e2d
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-10-05 19:29:39 -0600

    Bunch of changes: Data now sends to storage. Just need to get it back out.

--- script.js
+++ script.js
@@ -2,26 +2,32 @@
 // ESTABLISH CURRENT DAY //
 ///////////////////////////
 
-var todayDate = $("#currentDay");
 var todayDate = dayjs().format("YYYY, MM, DD")
+    todayDate.textContent = $("#currentDay");
 var todayHour = dayjs().format("YYYY, MM, DDT16:00:00.000Z")
 
-////////////////////////////////
-// LINKING TO CLASSES IN HTML //
-////////////////////////////////
+///////////////////////////////
+// ESTABLISH GLOBAL ELEMENTS //
+///////////////////////////////
 
-var containerDiv = $(".container")
-var timeBlockDiv = $(".timeBlock");
-var divRowDiv = $(".row");
-var hourDiv = $(".hour");
-var textAreaDiv = $(".description");
-var saveButton = $(".saveBtn");
+var newTimeBlock;
+var newRow;
+var newHour;
+var newTextArea;
+var newSave;
+var textInput;
+
+///////////////////
+// CREATE ARRAYS //
+///////////////////
+
+var allHours = [];
+var allText = [];
+var allSaves = [];
 
 ///////////////////////
 // GENERATE 24 HOURS //
 ///////////////////////
-
-var allHours = [];
 
 create24hours();
 
@@ -29,20 +35,20 @@ function create24hours(){
     // Set Daily Hours and AM/PM Variables 
 
         // Loop 24 Times
-        for ( var i=0 ; i<24 ; i++ ){
+        for ( var i=0 ; i<9 ; i++ ){
     
         //Determine AM or PM
         var amPM;
-        if (i < 11){
+        if ((i+8) < 11){
             amPM = " AM";
-            } else if (i >= 12) {
+            } else if ((i+8) >= 12) {
             amPM = " PM";
             };
         // Reset to 1 After 12
-        if (i > 11){
-            h = (i -12);
+        if ((i+8) > 11){
+            h = ((i+8) -12);
             } else {
-            h = i;
+            h = (i+8);
             }
 
         //Add Context to Each Time Stamp and Push to dailyHours Array
@@ -62,37 +68,106 @@ function create24hours(){
 
 function generateTimeBlocks(){
 
+    //Link to Container Element
+    var containerDiv = $(".container")
+
     //Loop Hour Array to Generate 24 Time Blocks
     for (var i = 0; i < allHours.length ; i++){
     
     // NEW TIME BLOCK DIV
-    var newTimeBlock = $("<div>");
+    newTimeBlock = $("<div>")
     newTimeBlock.attr("class", "time-block");
     containerDiv.append(newTimeBlock);
 
     // NEW ROW DIV
-    var newRow = $("<div>");
+    newRow = $("<div>");
     newRow.attr("class", "row");
     newTimeBlock.append(newRow);
 
     // NEW HOUR
-    var newHour = $("<div>" + allHours[i] + "</div>")
+    newHour = $("<div>" + allHours[i] + "</div>")
     newHour.attr("class", "hour");
     newRow.append(newHour);
 
     // NEW TEXTAREA 
-    var newTextArea = $("<textarea>");
+    newTextArea = $("<textarea>");
     newTextArea.attr("class", "description");
+    newTextArea.attr("data-idx", (i+8)+1);
     newHour.after(newTextArea);
     
     // NEW SAVE BUTTON 
-    var newSave = $("<button>");
+    newSave = $("<button>");
     newSave.attr("class", "saveBtn");
+    newSave.attr("data-idx", (i+8)+1);
     newSave.text("S");
     newTextArea.after(newSave);
 
+
     };
-    
-    renderTimeCode()
+
+
+////////////////////////////////////
+// LINK TO CLASSES IN NEW HTML //
+////////////////////////////////////
+
+var timeBlockDiv = $(".timeBlock");
+var divRowDiv = $(".row");
+var hourDiv = $(".hour");
+var textAreaDiv = $(".description");
+var saveButton = $(".saveBtn");
+
+
+    ////////////////////////
+    // COLLECT TEXT INPUT //
+    ////////////////////////
+
+    // When User Clicks Textbox...
+    textAreaDiv.on("click", function(e){
+        e.preventDefault();
+        console.log();
+
+    // Collect Textbox Number from Click
+        textNum = e.target.getAttribute("data-idx");
+        console.log(textNum);
+
+    ///////////////////
+    // SAVE FUNCTION //
+    ///////////////////
+        
+        // When User Clicks Save Button //
+        $(".saveBtn").on("click", function(e){
+        e.preventDefault();
+
+        // Collect Save Key from Click
+        var saveClick = e.target.getAttribute("data-idx");
+        console.log("Save Click: " + saveClick);
+
+            // If the Textbox Number Matches Save Key, Send Data to Storage
+            if (textNum = saveClick) {
+
+            // Collect User Input on Text
+            var textInput = $(this).siblings(".description").val();
+            console.log("Input Text :" + textInput);
+
+            // Send to Storage
+            console.log("VALUE: " + textInput);
+            console.log("KEY: " + saveClick);
+            localStorage.setItem(saveClick, textInput)
+
+            };
+        });
+
+    });
+
+
+
+
+
+
 
 };
+
+///////////////////////
+// PULL FROM STORAGE //
+///////////////////////
+
