444ac143303d12b4ebcb00e123c1c5457c12865b
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-10-06 18:01:46 -0600

    COMPLETE!

--- Develop/script.js
+++ Develop/script.js
@@ -2,279 +2,196 @@
 // ESTABLISH CURRENT DAY //
 ///////////////////////////
 
-// Generate Today //
-todayDate = $("#currentDay");
-var todayDate = dayjs().format("YYYY, MM, DD")
-var todayHour = dayjs().format("YYYY, MM, DDT16:00:00.000Z")
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-/////////////////////////////////
-// ESTABLISH VARIABLES IN HTML //
-/////////////////////////////////
+///////////////////////////////
+// ESTABLISH GLOBAL ELEMENTS //
+///////////////////////////////
 
-// Linking to Tags in HTML //
-var containerDiv = $(".container")
-var timeBlockDiv = $(".timeBlock");
-var divRowDiv = $(".row");
-var hourDiv = $(".hour");
-var textAreaDiv = $(".description");
-var saveButton = $(".saveBtn");
+var newTimeBlock;
+var newRow;
+var newHour;
+var newTextArea;
+var newSave;
+var textInput;
 
-/////////////////////////////////
-// CREATING ELEMENTS
-/////////////////////////////////
-var pTag = $("<p>");
-var divEl = $("<div>");
-var textEl = $("<textarea>");
-var saveEl = $("<button>")
-
-//////////////////////
-// TIME BLOCK OBJECT //
-//////////////////////
-
-timeBlockObject = {
-    hour : "",
-    description : "",
-    saveBtn : "",
-    textInput : "",
-    saveBtn : ""
-}
-
-//////////////////////
-// GENERATE 24 HOURS //
-//////////////////////
+///////////////////
+// CREATE ARRAYS //
+///////////////////
 
 var allHours = [];
-console.log(allHours)
+var allText = [];
+var allSaves = [];
 
-create24hours();
+///////////////////////
+// GENERATE WORK HOURS //
+///////////////////////
 
-function create24hours(){
-    // Set Daily Hours and AM/PM Variables 
+createWorkHours();
 
-        // Loop 24 Times
-        for ( var i=0 ; i<24 ; i++ ){
+// Establish Daily Hour Variables with Correct AM/PM
+function createWorkHours(){
+
+    // Loop 9 Times for 9 Work Hours
+    for ( var i=0 ; i<9 ; i = i+1 ){
+
+    //Determine AM or PM
+    var amPM;
+    if ((i+8) < 11){
+        amPM = " AM";
+        } else if ((i+8) >= 12) {
+        amPM = " PM";
+    };
     
-        //Determine AM or PM
-        var amPM;
-        if (i < 11){
-            amPM = " AM";
-            } else if (i >= 12) {
-            amPM = " PM";
-            };
-        // Reset to 1 After 12
-        if (i > 11){
-            h = (i -12);
-            } else {
-            h = i;
-            }
+    // Reset to 1 After 12
+    if ((i+8) > 11){
+        h = ((i+8) -12);
+        } else {
+        h = (i+8);
+    }
 
-        //Add Context to Each Time Stamp and Push to dailyHours Array
-        dailyHour = (h+1) + amPM;
-        console.log(dailyHour)
-        allHours.push(dailyHour);
+    //Add Context to Each Time Stamp and Push to dailyHours Array
+    dailyHour = (h+1) + amPM;
+    allHours.push(dailyHour);
          
     };
         
     generateTimeBlocks();
+
 };
 
-
-/////////////////////////////////
-// GENERATE 24 TIME BLOCKS //
-/////////////////////////////////
-
+////////////////////////////////////////////////////
+// GENERATE 9 TIME BLOCKS AND DROP ID'S FOR LATER //
+////////////////////////////////////////////////////
 
 function generateTimeBlocks(){
 
-    //Loop Hour Array to Generate 24 Time Blocks
+    //Link to Container Element
+    var containerDiv = $(".container")
+
+    //Loop Hour Array to Generate 9 Time Blocks
     for (var i = 0; i < allHours.length ; i++){
     
-    // NEW TIME BLOCK DIV
-    var newTimeBlock = $("<div>");
-    newTimeBlock.attr("class", "time-block");
-    containerDiv.append(newTimeBlock);
+        // NEW TIME BLOCK DIV
+        newTimeBlock = $("<div>")
+        newTimeBlock.attr("class", "time-block");
+        containerDiv.append(newTimeBlock);    
+            // Drop an ID for Color Coding Later
+            newTimeBlock.attr("id", (i+9) + "idBlock");
+        
 
-    // NEW ROW DIV
-    var newRow = $("<div>");
-    newRow.attr("class", "row");
-    newTimeBlock.append(newRow);
+        // NEW ROW DIV
+        newRow = $("<div>");
+        newRow.attr("class", "row");
+        newTimeBlock.append(newRow);
 
-    // NEW HOUR//
-    var newHour = $("<div>" + allHours[i] + "</div>")
-    newHour.attr("class", "hour");
-    console.log("Hours: " + newHour)
-    newRow.append(newHour);
+        // NEW HOUR
+        newHour = $("<div>" + allHours[i] + "</div>")
+        newHour.attr("class", "hour");
+        newRow.append(newHour);
 
-    // NEW TEXTAREA DIV
-    var newTextArea = $("<textarea>");
-    newTextArea.attr("class", "description");
-    newHour.after(newTextArea);
-    
-    // NEW BUTTON DIV
-    var newSave = $("<button>");
-    newSave.attr("class", "saveBtn");
-    newSave.text("S");
-    newTextArea.after(newSave);
+        // NEW TEXTAREA 
+        newTextArea = $("<textarea>");
+        newTextArea.attr("class", "description");
+        newHour.after(newTextArea);
+            // Drop an ID to Collect Click
+            newTextArea.attr("id", (i+9) + "id");
+        
+        
+        // NEW SAVE BUTTON 
+        newSave = $("<button>");
+        newSave.attr("class", "saveBtn");
+        newSave.text("S");
+        newTextArea.after(newSave);
+            // Drop Data to use for LocalStorage ID
+            newSave.attr("data-idx", (i+9));
 
+    };    
+
+////////////////////////
+// COLLECT TEXT INPUT //
+////////////////////////
+
+    // Link Variable to Description Section in HTML
+    var textAreaDiv = $(".description");
+
+        // When User Clicks a Text Box...
+        textAreaDiv.on("click", function(e){
+        e.preventDefault();
+
+        // Collect the ID Number from the Click
+            textNum = e.target.getAttribute("id");
+
+///////////////////
+// SAVE FUNCTION //
+///////////////////
+        
+            // When User Clicks Save Button...
+            $(".saveBtn").on("click", function(e){
+            e.preventDefault();
+
+            // Collect Save Key from Click
+            var saveClick = e.target.getAttribute("data-idx");
+
+                // If Text Box Number Matches its Save Button...
+                if (textNum = saveClick) {
+
+                    // Collect User Input on Text
+                    var textInput = $(this).siblings(".description").val();
+
+                    // Send Text Input and Save Key to Storage
+                    console.log("VALUE: " + textInput);
+                    console.log("KEY: " + saveClick);
+
+                };
+            });
+        });
+
+//////////////////////////////////////////////////
+// GET DATA FROM STORAGE TO PLACE IN TEXT BOXES //
+//////////////////////////////////////////////////
+
+    $("#9id").val(localStorage.getItem(9));
+    $("#10id").val(localStorage.getItem(10));
+    $("#11id").val(localStorage.getItem(11));
+    $("#12id").val(localStorage.getItem(12));
+    $("#13id").val(localStorage.getItem(13));
+    $("#14id").val(localStorage.getItem(14));
+    $("#15id").val(localStorage.getItem(15));
+    $("#16id").val(localStorage.getItem(16));
+    $("#17id").val(localStorage.getItem(17));
+
+////////////////////////////////////////
+// RENDER JS TIME CODES TO MATCH CODE //
+////////////////////////////////////////
+
+    colorCode();
+
+    function colorCode(){
+
+        // Parse the Current Hour from DayJS Function
+        var currentHour = parseInt(dayjs().format('H'));
+
+        // Loop Current Hour Against ID Blocks
+        for( var i=0; i<9; i++){
+            
+            // Apply Green to Future
+            if( (i+9) > currentHour){                          
+                $(`#${i+9}idBlock`).addClass("future");
+            
+            // Apply Grey to Past
+            } else if ((i+9) < currentHour){ 
+                $(`#${i+9}idBlock`).addClass("past");
+            
+            // Apply Red to Present
+            } else if ((i+9) === currentHour){        
+                console.log("present", i)                             
+                $(`#${i+9}idBlock`).addClass("present");
+            };
+        };
     };
-    
 };
 
-    
-    
-// var allHours = ["12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am", "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm"];
-
-
-// function create24hours(){
-//   // Set Daily Hours
-
-//     // Loop 24 Times
-//     for ( var i=0 ; i<24 ; i++ ){
-      
-//     //Add Context to Each Time Stamp and Push to dailyHours Array
-//     dailyHour = ((h+1) + ":00 " + amPM);
-//     allHours.push(dailyHour);
-//     };
-
-//     console.log(allHours)
-  
-// };
-
-
-
-
-
-
-// Generate Function from DayJS that Generates 24-Hour Array//
-// Pull from Array to Generate 24 Time Blocks
-// Create Click Events on Each Block Assigned to Objects
-// Create Save Button that Sends Objects to Storage
-// Create a Render Function that Rerenders the Timeblocks whenever it's saved.
-// Create a function for Before-and-After Color Functions Based on the DayJS and Object
-
-
-
-// Generate 24 Time Blocks
-//
-
-//
-// 1) Create Function to Render the State of the Array or List
-// 2) Create Function that Adds New ToDo to List
-// 3) Create addEventListener to Add ToDo When Submit //!!"Clicked",
-// 4) Make sure to send new Items through Render Function
-// 5) Create Function to Send Rendered Items to Storage
-// 6) Create Function that Gets Items from Storage
-// 7) Add Buttons that Eliminate Box When Its Complete - during render
-
-
-
-
-///////////////////////
-// GENERATE 24 HOURS // Note: I tried making a function for this but failed. (See Below Code)
-///////////////////////
-//var allHours = ["12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am", "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm"];
-
-// CREATE 24 HOURS //    
-// function create24hours(){
-//     // Set Daily Hours and AM/PM Variables 
-    
-//         // Loop 24 Times
-//         for ( var i=0 ; i<24 ; i++ ){
-//         // Determine AM or PM
-//         if (i < 11){
-//             amPM = "am";
-//             } else if (i >= 12) {
-//             amPM = "pm";
-//             };
-//         // Reset to 1 After 12
-//         if (i > 11){
-//             h = (i -12);
-//             } else {
-//             h = i;
-//             }
-    
-//         //Add Context to Each Time Stamp and Push to dailyHours Array
-//         dailyHour = ((h+1) + ":00 " + amPM);
-//         allHours.push(dailyHour);
-//         };
-//         console.log(allHours)
-//         generateContainer()
-    
-//     };
-
-
-
-
-
-
-// /////////////////////////////////
-// // 24-HOURS ARRAY //
-// /////////////////////////////////
-// allHours = ["Hour1", "Hour2", "Hour3", "Hour4", "Hour5", "Hour6", "Hour7", "Hour8", "Hour9", "Hour10", "Hour11", "Hour12", "Hour13", "Hour14", "Hour15", "Hour16", "Hour17", "Hour18", "Hour19", "Hour20", "Hour21", "Hour22", "Hour23", "Hour24"];
-// console.log(allHours.length)
-
-// var hourEl;
-
-
-
-
-
-// ////////////////////////
-// // GENERATE TIME STAMPS //
-// ////////////////////////
-
-// for( i=0 ; i<24 ; i++){
-
-// };
-
-
-// allHours.forEach(function(dailyHour){
-// hourButton = $("<button class='hour'>");
-// hourButton.text(dailyHour);
-
-// })
-
-
-
-
-//colDiv.append("rowDiv").addClass("row");
-//$("rowDiv").append("Why won't this work?");
-
-
-
-
-
-
-
-
-// // CREATE 24 HOURS //
-// if (allHours.length < 24){
-// create24hours()
-// };
-
-// function create24hours(){
-//   // Set Daily Hours and AM/PM Variables 
-
-//     // Loop 24 Times
-//     for ( var i=0 ; i<24 ; i++ ){
-//       // Determine AM or PM
-//       if (i < 11){
-//         amPM = "am";
-//         } else if (i >= 12) {
-//           amPM = "pm";
-//         };
-//       // Reset to 1 After 12
-//       if (i > 11){
-//         h = (i -12);
-//         } else {
-//           h = i;
-//         }
-
-//     //Add Context to Each Time Stamp and Push to dailyHours Array
-//     dailyHour = ((h+1) + ":00 " + amPM);
-//     allHours.push(dailyHour);
-//     };
-//     console.log(allHours)
-//     generateContainer()
-  
-// };
+/////////
+// FIN //
+/////////
